38ece06362d19f1a5b5b687b31dd9011ea24403e
Olena1iashenko <155830179+Olena1iashenko@users.noreply.github.com>
2024-04-11 22:02:58 +0000

    Deploying to gh-pages from @ Olena1iashenko/goit-js-hw-11@3ab42a877da526a0f7e3710f094c2ca609e26b17 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,7 +1,7 @@
 import{S as d,i as n}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const p=s=>{const t="https://pixabay.com",o="43337272-ad726c9b9e29498af38112cb0",a=new URLSearchParams({q:s,image_type:"photo",orientation:"horizontal",safesearch:"true",key:o});return fetch(`${t}/api/?${a}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()})},f=s=>s.map(t=>`
       <div class="card-wrapper">
   <li class="gallery-card">
-    <a href="${t.largeImageUR}"><img src="${t.webformatURL}" alt="${t.tags}" class="gallery-img" /></a>
+    <a href="${t.largeImageURL}"><img src="${t.webformatURL}" alt="${t.tags}" class="gallery-img"/></a>
     <div class="gallery-panel">
       <p>Likes ${t.likes}</p>
       <p>Views ${t.views}</p>
